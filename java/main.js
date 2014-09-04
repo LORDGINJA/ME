a2761e6b9cd152b62ac8e0f0dfc7a21f8192aae1
Maxwell Karp <maxwell.karp@yahoo.com>
2014-09-04 10:56:28 -0700

    update on my portfolio project

---
+++ java/main.js
@@ -0,0 +1,11 @@
+$('.menu a').click(function(){
+ 
+  $('.trigger').toggle();
+  $('.menu').toggleClass('round');
+  $('.close').toggle();
+  $('.drop-down').toggleClass('down');
+  
+ 
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////

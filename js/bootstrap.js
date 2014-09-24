628806adbaaa59f1e0150f75ffe5a7d486a74b2b
Maxwell Karp <maxwell.karp@yahoo.com>
2014-09-23 23:12:44 -0700

    Carousel stuff

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -303,7 +303,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   Carousel.VERSION  = '3.2.0'
 
   Carousel.DEFAULTS = {
-    interval: 5000,
+    interval: 3000,
     pause: 'hover',
     wrap: true
   }
